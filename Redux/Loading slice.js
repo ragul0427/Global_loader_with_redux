04a3f280ac3d16625526c6af2289cbc5882a60a5
ragul0427 <121870806+ragul0427@users.noreply.github.com>
2023-05-24 09:49:54 +0530

    Update Loading slice.js

--- Redux/Loading slice.js
+++ Redux/Loading slice.js
@@ -1,17 +1,4 @@
-// loadingSlice.js
-import { createSlice } from '@reduxjs/toolkit';
 
-const loadingSlice = createSlice({
-  name: 'loading',
-  initialState: false,
-  reducers: {
-    startLoading: (state) => true,
-    stopLoading: (state) => false,
-  },
-});
-
-export const { startLoading, stopLoading } = loadingSlice.actions;
-export default loadingSlice.reducer;
 // src/redux/loaderSlice.js
 
 import { createSlice } from '@reduxjs/toolkit';
